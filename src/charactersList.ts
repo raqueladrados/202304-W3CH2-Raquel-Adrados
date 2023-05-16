076f131ee363e02924b890fad33f13c1f5e7c26a
Raquel Adrados <radrados00@gmail.com>
2023-05-16 18:46:33 +0200

    Add classes and tempaltes

---
+++ src/charactersList.ts
@@ -0,0 +1,23 @@
+import { Character } from "./character";
+import { King } from "./king";
+import { Counselor } from "./counselor";
+import { Squire } from "./squire";
+import { Fighter } from "./fighter";
+
+export const createCharacterList = () => {
+  const joffrey = new King("Joffrey", "Baratheon", 18, 3);
+  const jamie = new Fighter("Jamie", "Lannister", 33, "Espada");
+  const daenerys = new Fighter("Daenerys", "Targaryen", 32, "Dragones");
+  const tyrion = new Counselor("Tyrion", "Lannister", 35, daenerys);
+  const broon = new Squire("Bronn", "Unknown", 40, jamie, 1);
+
+  const characterList = [];
+
+  characterList.push(joffrey);
+  characterList.push(jamie);
+  characterList.push(daenerys);
+  characterList.push(tyrion);
+  characterList.push(broon);
+
+  return characterList;
+};
---
+++ src/templatesCharacters.ts
@@ -0,0 +1,123 @@
+import { King } from "./king";
+import { Counselor } from "./counselor";
+import { Squire } from "./squire";
+import { Fighter } from "./fighter";
+
+export const kingTemplate = (object: King) => ` <li class="character col">
+        <div class="card character__card">
+          <img src="img/${object.name}.jpg" alt="${object.name} ${object.family}" class="character__picture card-img-top" />
+          <div class="card-body">
+            <h2 class="character__name card-title h4">${object.name} ${object.family}</h2>
+            <div class="character__info">
+              <ul class="list-unstyled">
+                <li>Edad: ${object.age}</li>
+                <li>
+                  Estado:
+                  <i class="fas fa-thumbs-down"></i>
+                  <i class="fas fa-thumbs-up"></i>
+                </li>
+              </ul>
+            </div>
+            <div class="character__overlay">
+              <ul class="list-unstyled">
+                <li>Años de reinado: ${object.reignLength}</li>
+              </ul>
+              <div class="character__actions">
+                <button class="character__action btn">habla</button>
+                <button class="character__action btn">muere</button>
+              </div>
+            </div>
+          </div>
+          <i class="emoji"></i>
+        </div>
+      </li>`;
+
+export const FighterTemplate = (object: Fighter) => ` <li class="character col">
+        <div class="card character__card">
+          <img src="img/${object.name}.jpg" alt="${object.name} ${object.family} del personaje" class="character__picture card-img-top" />
+          <div class="card-body">
+            <h2 class="character__name card-title h4">${object.name} ${object.family}</h2>
+            <div class="character__info">
+              <ul class="list-unstyled">
+                <li>Edad: ${object.age}</li>
+                <li>
+                  Estado:
+                  <i class="fas fa-thumbs-down"></i>
+                  <i class="fas fa-thumbs-up"></i>
+                </li>
+              </ul>
+            </div>
+            <div class="character__overlay">
+              <ul class="list-unstyled">
+                <li>Arma: ${object.weapon}</li>
+              </ul>
+              <div class="character__actions">
+                <button class="character__action btn">habla</button>
+                <button class="character__action btn">muere</button>
+              </div>
+            </div>
+          </div>
+          <i class="emoji"></i>
+        </div>
+      </li>`;
+
+export const counselorTemplate = (
+  object: Counselor
+) => `<li class="character col">
+        <div class="card character__card">
+          <img src="img/${object.name}.jpg" alt="${object.name} ${object.family} del personaje" class="character__picture card-img-top" />
+          <div class="card-body">
+            <h2 class="character__name card-title h4">${object.name} ${object.family}</h2>
+            <div class="character__info">
+              <ul class="list-unstyled">
+                <li>Edad: ${object.age}</li>
+                <li>
+                  Estado:
+                  <i class="fas fa-thumbs-down"></i>
+                  <i class="fas fa-thumbs-up"></i>
+                </li>
+              </ul>
+            </div>
+            <div class="character__overlay">
+              <ul class="list-unstyled">
+                <li>Asesora a: ${object.support.name}</li>
+              </ul>
+              <div class="character__actions">
+                <button class="character__action btn">habla</button>
+                <button class="character__action btn">muere</button>
+              </div>
+            </div>
+          </div>
+          <i class="emoji"></i>
+        </div>
+      </li>`;
+
+export const squireTemplate = (object: Squire) => `<li class="character col">
+        <div class="card character__card">
+          <img src="img/${object.name}.jpg" alt="${object.name} ${object.family} del personaje" class="character__picture card-img-top" />
+          <div class="card-body">
+            <h2 class="character__name card-title h4">${object.name} ${object.family}</h2>
+            <div class="character__info">
+              <ul class="list-unstyled">
+                <li>Edad: ${object.age}</li>
+                <li>
+                  Estado:
+                  <i class="fas fa-thumbs-down"></i>
+                  <i class="fas fa-thumbs-up"></i>
+                </li>
+              </ul>
+            </div>
+            <div class="character__overlay">
+              <ul class="list-unstyled">
+                <li>Peloteo: ${object.pelotismo}</li>
+                <li>Sirve a: ${object.support.name}</li>
+              </ul>
+              <div class="character__actions">
+                <button class="character__action btn">habla</button>
+                <button class="character__action btn">muere</button>
+              </div>
+            </div>
+          </div>
+          <i class="emoji"></i>
+        </div>
+      </li>`;
